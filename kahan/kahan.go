12c07f182cc9706034afac9dba2935a73af314f0
Alex Nichol <unixpickle@gmail.com>
2016-03-26 18:16:46 -0400

    created rolling kahan summer
    This is good for times when it would be inefficient to allocate and store all of the summands at once.

--- kahan/kahan.go
+++ kahan/kahan.go
@@ -1,15 +1,35 @@
 package kahan
 
+// A Summer64 computes a rolling sum of float64s.
+type Summer64 struct {
+	sum          float64
+	compensation float64
+}
+
+// NewSummer64 creates a Summer64 with a starting sum of 0.
+func NewSummer64() *Summer64 {
+	return &Summer64{}
+}
+
+// Add adds a number to the current sum, returning the new sum.
+func (s *Summer64) Add(n float64) float64 {
+	n -= s.compensation
+	sum := s.sum + n
+	s.compensation = (sum - s.sum) - n
+	s.sum = sum
+	return s.sum
+}
+
+// Sum returns the current sum.
+func (s *Summer64) Sum() float64 {
+	return s.sum
+}
+
+// Sum64 adds all the floats in a slice and returns the sum
 func Sum64(nums []float64) float64 {
-	var sum float64
-	var compensation float64
-
-	for _, num := range nums {
-		num -= compensation
-		roundedSum := sum + num
-		compensation = (roundedSum - sum) - num
-		sum = roundedSum
+	var summer Summer64
+	for _, n := range nums {
+		summer.Add(n)
 	}
-
-	return sum
+	return summer.Sum()
 }
