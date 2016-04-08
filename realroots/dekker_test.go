ac96014f3f191825aa972dd3e4d3ae3643bf2fb8
Alex Nichol <unixpickle@gmail.com>
2016-04-08 09:10:04 -0400

    crafted malicious Dekker case
    The function 2^-x really screws over the secant method, because each time you go to the right by 1, you go down by a factor of two. In other words, each secant is twice as close to the axis, but always sloped in such a way that the next approximate root is the previous approximation + 1.

--- realroots/dekker_test.go
+++ realroots/dekker_test.go
@@ -11,6 +11,19 @@ func (_ sineFunction) Eval(x float64) float64 {
 	return math.Sin(x)
 }
 
+type badDekker struct {
+	callCount int
+}
+
+func (b *badDekker) Eval(x float64) float64 {
+	b.callCount++
+	if x <= 60 {
+		return math.Pow(2, -x)
+	} else {
+		return math.Pow(2, -60) + -60*(x-60)
+	}
+}
+
 func TestDekker(t *testing.T) {
 	d := newDekker(sineFunction{}, Interval{1, 4})
 	bound := 1e-11
@@ -23,5 +36,20 @@ func TestDekker(t *testing.T) {
 	root := d.Root()
 	if math.Abs(root-math.Pi) > 1.5*bound {
 		t.Error("expected", math.Pi, "got", root, "(using bound ", bound, ")")
+	}
+
+	bad := &badDekker{}
+	d = newDekker(bad, Interval{0, 61})
+	for {
+		d.Step()
+		if d.Bounded(1e-7) {
+			break
+		}
+	}
+	if bad.callCount < 120 {
+		t.Error("unexpected call count", bad.callCount)
+	}
+	if math.Abs(d.Root()-60) > 1e-6 {
+		t.Error("expected root of ~60 but got", d.Root())
 	}
 }
