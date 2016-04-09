035508268c569d0b3e80cdc21fad5c02d79d6968
Alex Nichol <unixpickle@gmail.com>
2016-04-09 12:16:08 -0400

    added ComplexSummer128 to kahan

--- kahan/kahan.go
+++ kahan/kahan.go
@@ -33,3 +33,31 @@ func Sum64(nums []float64) float64 {
 	}
 	return summer.Sum()
 }
+
+// A ComplexSummer128 computes a rolling sum of complex128s.
+type ComplexSummer128 struct {
+	realSum *Summer64
+	imagSum *Summer64
+}
+
+// NewComplexSummer128 creates a new ComplexSummer128
+// with a starting value of 0.
+func NewComplexSummer128() *ComplexSummer128 {
+	return &ComplexSummer128{
+		realSum: NewSummer64(),
+		imagSum: NewSummer64(),
+	}
+}
+
+// Add adds a number to the current sum, returning
+// the new sum.
+func (c *ComplexSummer128) Add(n complex128) complex128 {
+	c.realSum.Add(real(n))
+	c.imagSum.Add(imag(n))
+	return c.Sum()
+}
+
+// Sum returns the current sum.
+func (c *ComplexSummer128) Sum() complex128 {
+	return complex(c.realSum.Sum(), c.imagSum.Sum())
+}
