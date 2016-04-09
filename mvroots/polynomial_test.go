951cef952d80fb3c0287ca6dfdc6cfed61fa5829
Alex Nichol <unixpickle@gmail.com>
2016-04-09 17:43:16 -0400

    complex polynomial root finding passes

---
+++ mvroots/polynomial_test.go
@@ -0,0 +1,52 @@
+package mvroots
+
+import (
+	"math/cmplx"
+	"testing"
+)
+
+func TestPolynomialRootsRealCoeffs(t *testing.T) {
+	poly := Polynomial{3, -1, 1, -3, 4, 5}
+	roots := []complex128{
+		-1.4818635,
+		-0.3241806 + 0.7199509i,
+		-0.3241806 - 0.7199509i,
+		0.6651123 + 0.4550801i,
+		0.6651123 - 0.4550801i,
+	}
+	generalPolynomialTest(t, poly, roots, 1e-5)
+}
+
+func TestPolynomialRootsComplexCoeffs(t *testing.T) {
+	poly := Polynomial{(3 + 2i), (1 - 3i), (15 - 0.5i), -(1 + 4i), (0.1 + 5i), (0.1 + 0.2i)}
+	roots := []complex128{
+		-20.938812713787172 - 9.322132020458058i,
+		1.674619725670770 + 0.983776748528890i,
+		-0.883651066344169 - 1.382478799221257i,
+		-0.182747574812801 + 0.508194313349658i,
+		0.130591629273395 - 0.387360242199228i,
+	}
+	generalPolynomialTest(t, poly, roots, 1e-7)
+}
+
+func generalPolynomialTest(t *testing.T, p Polynomial, roots []complex128, prec float64) {
+	actual := p.Roots()
+	expected := map[complex128]bool{}
+	for _, x := range roots {
+		expected[x] = true
+	}
+
+ActualLoop:
+	for _, x := range actual {
+		for exp := range expected {
+			if cmplx.Abs(exp-x) < prec {
+				delete(expected, exp)
+				continue ActualLoop
+			}
+		}
+		t.Error("incorrect root:", x)
+	}
+	if len(expected) > 0 {
+		t.Error("missing roots:", expected)
+	}
+}
