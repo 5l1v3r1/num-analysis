f2875fd17f339492f0197582d684932ab8dbe285
Alex Nichol <unixpickle@gmail.com>
2016-04-09 12:59:47 -0400

    drafted interface for multivariable functions

---
+++ mvroots/func.go
@@ -0,0 +1,92 @@
+package mvroots
+
+import (
+	"github.com/unixpickle/num-analysis/kahan"
+	"github.com/unixpickle/num-analysis/linalg"
+)
+
+// Func is a multivariable, differentiable function
+// with the same number of inputs and outputs.
+type Func interface {
+	// Dim returns the function's dimensionality.
+	// Number of inputs = number of outputs = Dim().
+	Dim() int
+
+	// Eval evaluates the function at a given
+	// input vector.
+	Eval(input linalg.Vector) linalg.Vector
+
+	// Jacobian computes the Jacobian at a given
+	// input vector.
+	// Each row of the Jacobian corresponds to a
+	// component of the output, and each column
+	// corresponds to a component of the input.
+	Jacobian(input linalg.Vector) *linalg.Matrix
+}
+
+// Polynomial is a complex-valued polynomial which
+// is treated as a multivariable function.
+// The input and output vectors are of the form
+// [real, imaginary].
+type Polynomial []complex128
+
+// Dim returns 2, since there are two real
+// components to each complex number.
+func (p Polynomial) Dim() int {
+	return 2
+}
+
+// Eval treats the input vector as a complex
+// number, evaluates the polynomial, and then
+// returns the result as a vector.
+func (p Polynomial) Eval(vec linalg.Vector) linalg.Vector {
+	if len(vec) != 2 {
+		panic("wrong dimensionality")
+	}
+
+	x := complex(vec[0], vec[1])
+
+	xPower := complex128(1)
+	s := kahan.NewComplexSummer128()
+	for _, a := range p {
+		s.Add(a * xPower)
+		xPower *= x
+	}
+
+	sum := s.Sum()
+	return linalg.Vector{real(sum), imag(sum)}
+}
+
+// Jacobian computes the partial derivatives of
+// the real and complex parts of the polynomial
+// with respect to the real and complex values
+// of the input.
+func (p Polynomial) Jacobian(vec linalg.Vector) *linalg.Matrix {
+	if len(vec) != 2 {
+		panic("wrong dimensionality")
+	}
+
+	x := complex(vec[0], vec[1])
+	res := linalg.NewMatrix(2, 2)
+
+	realDeriv := p.evaluateDerivative(x)
+	res.Set(0, 0, real(realDeriv))
+	res.Set(1, 0, imag(realDeriv))
+
+	imagDeriv := realDeriv * 1i
+	res.Set(0, 1, real(imagDeriv))
+	res.Set(1, 1, imag(imagDeriv))
+
+	return res
+}
+
+func (p Polynomial) evaluateDerivative(x complex128) complex128 {
+	xPower := complex128(1)
+	s := kahan.NewComplexSummer128()
+	for i, coeff := range p[1:] {
+		diffCoeff := complex(float64(i+1), 0) * coeff
+		s.Add(diffCoeff * xPower)
+		xPower *= x
+	}
+	return s.Sum()
+}
