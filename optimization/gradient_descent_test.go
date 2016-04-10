68d4facf17a6302851cc193823acae9e97b67663
Alex Nichol <unixpickle@gmail.com>
2016-04-10 18:11:20 -0400

    test gradient descent for singular matrix equation

--- optimization/gradient_descent_test.go
+++ optimization/gradient_descent_test.go
@@ -26,3 +26,46 @@ func TestGradientDescentSolvableSystem(t *testing.T) {
 		t.Error("expected", expected, "but got", actual)
 	}
 }
+
+func TestGradientDescentProjection(t *testing.T) {
+	matrix := &linalg.Matrix{
+		Rows: 4,
+		Cols: 3,
+		Data: []float64{
+			1, 2, 3,
+			5, 6, 7,
+			3, 4, 1,
+			8, 9, 10,
+		},
+	}
+	product := linalg.Vector{1, 2, 3, 4}
+	sys := NewLinSysFunc(matrix, product)
+	actual := GradientDescent(sys, 0)
+	expected := linalg.Vector{-0.300675675675677, 1.060810810810812, -0.341216216216216}
+	diff := actual.Copy().Add(expected.Copy().Scale(-1))
+	if diff.Dot(diff) > 1e-10 {
+		t.Error("expected", expected, "but got", actual)
+	}
+}
+
+func TestGradientDescentProjectionMany(t *testing.T) {
+	matrix := &linalg.Matrix{
+		Rows: 4,
+		Cols: 4,
+		Data: []float64{
+			1, 2, 3, 1,
+			5, 6, 7, 5,
+			3, 4, 1, 3,
+			8, 9, 10, 8,
+		},
+	}
+	product := linalg.Vector{1, 2, 3, 4}
+	sys := NewLinSysFunc(matrix, product)
+	answer := GradientDescent(sys, 0)
+	actualProduct := linalg.Vector(matrix.Mul(linalg.NewMatrixColumn(answer)).Data)
+	expectedProduct := linalg.Vector{0.797297297297297, 2.472972972972972, 3, 3.729729729729727}
+	diff := actualProduct.Copy().Add(expectedProduct.Copy().Scale(-1))
+	if diff.Dot(diff) > 1e-10 {
+		t.Error("expected", expectedProduct, "but got", actualProduct)
+	}
+}
