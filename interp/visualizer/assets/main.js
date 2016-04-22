4abff30adf31b2f94f5d287c9cfdb32b0220e9cc
Alex Nichol <unixpickle@gmail.com>
2016-04-22 14:33:00 +0000

    draw basic polynomial interpolation

--- interp/visualizer/assets/main.js
+++ interp/visualizer/assets/main.js
@@ -1,11 +1,24 @@
 (function() {
 
   var drawing = null;
+  var INTERP_STEP = 0.005;
+  var INTERP_COUNT = 201;
 
   function initialize() {
+    drawing = new window.Drawing();
+    drawing.onPointAdded = interpolate;
   }
 
   function interpolate() {
+    var xs = drawing.getPointXs();
+    var ys = drawing.getPointYs();
+    var interpXs = [];
+    for (var i = 0; i < INTERP_COUNT; ++i) {
+      interpXs.push(i*INTERP_STEP);
+    }
+    var interpYs = window.interpolate('poly', xs, ys, 0, INTERP_STEP, INTERP_COUNT);
+    console.log(interpYs);
+    drawing.setInterpolatedLine(interpXs, interpYs);
   }
 
   $(initialize);
